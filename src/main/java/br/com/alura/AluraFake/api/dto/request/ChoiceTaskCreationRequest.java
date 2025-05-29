1a9dc1746555a7112e8d6d6983a3ad46523bcb2b
DhenSouza <denilson_contato@outlook.com>
2025-05-29 10:51:36 -0300

    feat: implementando a inserção das tarefas multipla escolhas e remodelando a escolha unica para uma forma abstrata por compartilhar a mesma logica da inserção, tambem removi interface que nao sera mais utilizada na solução

---
+++ src/main/java/br/com/alura/AluraFake/api/dto/request/ChoiceTaskCreationRequest.java
@@ -0,0 +1,15 @@
+package br.com.alura.AluraFake.api.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public interface ChoiceTaskCreationRequest extends TaskCreationRequest {
+
+    @NotEmpty
+    @Size(min = 2, max = 5)
+    @Valid
+    List<ChoiceOptionRequest> options();
+}
